ab727df3a236bd310a8b449b1f3bf41ae3d02012
Arman <arman@solutionart.net>
2022-02-26 10:06:04 +0600

    cs

--- ShopManagementUI/src/config.js
+++ ShopManagementUI/src/config.js
@@ -1,6 +1,7 @@
 // export const apiHostName = 'http://192.168.0.102:5001';
 // export const apiHostName = `${window.location.protocol}//${window.location.hostname}:5001`;
-export const apiHostName = 'https://localhost:44347';
+// export const apiHostName = 'https://localhost:44347';
+export const apiHostName = 'http://mansurtraders-001-site1.gtempurl.com/';
 export const host = 'http://localhost:3008'
 export const adminApiUrl = "http://localhost:8083";
 export const authority = "http://localhost:8082";
