fa0ef345ee4ff39548061ccb3a1298f1a1c255d8
Arman <arman@solutionart.net>
2022-06-14 02:11:15 +0600

    price

---
+++ ShopManagementApi/ShopManagement/ShopManagement.Data/Migrations/20220613195141_notNullPrice.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShopManagement.Data.Migrations
+{
+    public partial class notNullPrice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "OpeningStocks",
+                type: "decimal(15, 2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(15, 2)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "OpeningStocks",
+                type: "decimal(15, 2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(15, 2)");
+        }
+    }
+}
